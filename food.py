c11bcf63b65e26b4c6ed8f2459a0b5d9a42b5e43
mathconnor <rconnor@rutgers.com>
2019-09-10 23:43:20 -0400

    add missing bracket to value, calories ordered pair

--- food.py
+++ food.py
@@ -14,7 +14,7 @@ class Food(object):
         return self.getValue()/self.getCost()
 
     def __str__(self):
-        return self.name + ': <' + str(self.value) + ', '  + str(self.calories)
+        return self.name + ': <' + str(self.value) + ', ' + str(self.calories) + '>'
 
 def buildMenu(names, values, calories):
     """
@@ -38,10 +38,30 @@ def greedy(items, maxCost, keyFunction):
     itemsCopy = sorted(items, key=keyFunction, reverse=True)
     result =[]
     totalValue, totalCost = 0.0, 0.0
-    
+
     for i in range(len(itemsCopy)):
         if (totalCost + itemsCopy[i].getCost()) <= maxCost:
             result.append(itemsCopy[i])
             totalCost += itemsCopy[i].getCost()
             totalValue += itemsCopy[i].getValue()
     return (result, totalValue)
+
+def testGreedy(items, constraint, keyFunction):
+    taken, val = greedy(items, constraint, keyFunction)
+    print("Total value of items taken = {}".format(val))
+    for item in taken:
+        print('   ', item)
+
+def testGreedys(foods, maxUnits):
+    print('Use greedy by value to allocate', maxUnits, 'calories')
+    testGreedy(foods, maxUnits, Food.getValue)
+    print('Use greedy by cost to allocate', maxUnits, 'calories')
+    testGreedy(foods, maxUnits, lambda x: 1/Food.getCost(x))
+    print('Use greedy by cost to allocate', maxUnits, 'calories')
+    testGreedy(foods, maxUnits, Food.density)
+
+names = ['wine', 'beer', 'pizza', 'burger', 'fries', 'cola', 'appple', 'donut', 'cake']
+values = [89, 90, 95, 100, 90, 79, 50, 10]
+calories = [123, 154, 358, 354, 365, 150, 95, 195 ]
+foods = buildMenu(names, values, calories)
+testGreedys(foods, 750)
